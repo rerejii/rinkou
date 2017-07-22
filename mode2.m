0555e9f26cc65c950c255c270429002e762e6faf
rerejii <hayakawa.shinya.kochi@gmail.com>
2017-07-22 13:30:49 +0900

    start

--- mode2.m
+++ mode2.m
@@ -1,5 +1,5 @@
-%p-タイル法
-%読み込む画像の切り取る領域を58%と仮定
+%モード法
+%ウサギと背景が
 
 %-----初期化処理-----
 clear;
@@ -12,14 +12,23 @@ g = double(img(:,:,2));              %G値取得
 b = double(img(:,:,3));              %B値取得
 %-----グレースケール化-----
 gray = 0.3*r+0.59*g+0.11*b;          %グレイスケール化
-%-----降順ソート-----
-sdata = sort(gray(:),'descend');     %降順にソート
-%-----閾値設定-----
-p = 0.58                             %p値を58%と指定する
-t = sdata(round((x*y)*p));           %39150番目の画素値を閾値にする
+
+max_t = 0;
+min_val = length(gray);
+stock = zeros(1, 256);
+count = 0;
+for t = 0:255
+    result = (-0.000006*(t.^4))+(0.003*(t.^3))+(-0.5016*(t.^2))+(24.465*t)+196;
+    stock(t+1) = result;
+    count = count + 1;
+    if (min_val > result)
+        min_val = result;
+        max_t = t;
+    end
+end
 %-----2値化処理-----
-two_color = zeros(y,x);              %ゼロ配列を作成
-two_color(gray>=t) = 255;            %閾値以下の画素値に255(白)を格納
+two_color = zeros(y,x);                %ゼロ配列を作成
+two_color(gray>=max_t) = 255;          %閾値以下の画素値に255(白)を格納
 %-----画像表示-----
 figure(4);
 imshow(two_color);
